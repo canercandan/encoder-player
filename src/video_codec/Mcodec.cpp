64774fe57a72cf67e5904258350fa1edf13b05e6
derame_g <derame_g>
2010-07-01 17:06:24 +0000

    retee

--- src/video_codec/Mcodec.cpp
+++ src/video_codec/Mcodec.cpp
@@ -1,5 +1,6 @@
 
 #include "Mcodec.h"
+#include <stdio.h>
 
 Mcodec::Mcodec()
 {}
@@ -28,7 +29,7 @@ void				Mcodec::dislay_picture(IplImage *image)
     cvDestroyWindow("mwindow");
 }
 
-void				Mcodec::compressImage(std::string image_path)
+Image				Mcodec::compressImage(std::string image_path)
 {
     IplImage		*image;
     IplImage		*bloc;
@@ -62,35 +63,43 @@ void				Mcodec::compressImage(std::string image_path)
 			}
 		}
 	}
-    VC.SaveImgInList(this->ImgRec,width,height);
-    delete(this->ImgRec);
-    VC.createFile("test.GuiGuy");
-    VC.SaveFlux();
+    	Image						ObjImage;
+	ObjImage.setHeight(height);
+	ObjImage.setWidth(width);
+	ObjImage.setTab(this->ImgRec,width,height);
+	delete(this->ImgRec);
+	return(ObjImage);
+
+    
+       
 }
 
 void								Mcodec::uncompressImage()
 {
 	VideoCodec						vc;
-	Image							infos; //Modification du type de infos (objet image)
+	//	Image							infos; //Modification du type de infos (objet image)
 	short int						*values;
-	unsigned char					*datas;
-	DCT								dct;
+	unsigned char						*datas;
+	DCT							dct;
 	Quantizer						qt;
-	int								height;
-	int								width;
+	int							height;
+	int							width;
 	IplImage						*image;
+	std::list<Image>					infos
 
-	std::cout << "video codec decompression ()" << std::endl;
+	
+
+	  /*	std::cout << "video codec decompression ()" << std::endl;
 	vc.decompression();
 	std::cout << "lecture du fichier" << std::endl;
 	infos = vc.lectureFichier();
 	height = infos.getHeight(); //recuperation de la heuteur
 	width = infos.getWidth(); //recuperation de la largeur
-	if ((height * width) % 8)
+     	if ((height * width) % 8)
 		return ;
 	image = cvCreateImage(cvSize(height, width),IPL_DEPTH_8U,1);
 	std::cout << "conversion tableau begin." << std::endl;
-	values = this->twoDimensionToOneDimension(infos.getTab(), height, width); //on passe directement le tableau grace a l'objet infos
+	values = this->twoDimensionToOneDimension(test, height, width);
 	std::cout << "conversion tableau end." << std::endl;
 	datas = (uchar *) image->imageData;
 	std::cout << "reverse dct and quantization starting." << std::endl;
@@ -101,7 +110,7 @@ void								Mcodec::uncompressImage()
 	}
 	this->dislay_picture(image);
 	std::cout << "reverse dct and quantization complete." << std::endl;
-	delete (values);
+	delete (values);*/
 }
 
 short int					*Mcodec::twoDimensionToOneDimension(int **tab_src, int height, int width)
@@ -109,9 +118,11 @@ short int					*Mcodec::twoDimensionToOneDimension(int **tab_src, int height, int
 	short int				*tab_dst;
 	
 	tab_dst = new short int[height * width];
-	for (int x = 0; x < height; x++)
-		for (int y = 0; y < width; y++)
-			tab_dst[x * width + y] = (short int) tab_src[x][y];
+	for (int x = 0; x < width; x++)
+		for (int y = 0; y < height; y++)
+		  {
+		    tab_dst[x * height + y] = (short int) tab_src[x][y];
+		  }
 	return (tab_dst);
 }
 
